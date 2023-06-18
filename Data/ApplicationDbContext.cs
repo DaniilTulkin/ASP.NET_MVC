251b57279c792772626431ac55162d60e836965f
DaniilTulkin <tulkindaniilal@gmail.com>
2023-06-18 23:09:25 +0300

    db context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ASP.NET_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
