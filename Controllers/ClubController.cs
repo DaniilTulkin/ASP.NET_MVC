e57131ec7badfe3674940417dd196097d1dd08cc
DaniilTulkin <tulkindaniilal@gmail.com>
2023-06-20 12:46:08 +0300

    Views

--- Controllers/ClubController.cs
+++ Controllers/ClubController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP.NET_MVC.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_MVC.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var clubs = context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
--- Controllers/RaceController.cs
+++ Controllers/RaceController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP.NET_MVC.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_MVC.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var races = context.Races.ToList();
+            return View(races);
         }
     }
 }
